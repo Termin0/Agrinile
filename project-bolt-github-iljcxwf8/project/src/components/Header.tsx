26bea1fd23920010a4fd7c1ceafed9d536287a5a
Abdelrahman Yasser <99607171+Termin0@users.noreply.github.com>
2025-09-25 12:19:02 +0300

    Delete project-bolt-github-iljcxwf8/project directory

--- project-bolt-github-iljcxwf8/project/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-@@ .. @@
-           {/* Logo */}
-           <div className="flex items-center">
-             <img 
--              src="/public/WhatsApp Image 2025-08-23 at 23.02.03_6b3039bb.jpg" 
-+              src="/0.png" 
-               alt="Agrinile Logo" 
-               className="h-10 w-auto"
-             />
--- project-bolt-github-iljcxwf8/project/src/components/Hero.tsx
+++
@@ -1,8 +0,0 @@
-@@ .. @@
-         <div className="mb-8">
-           <img 
--            src="/public/WhatsApp Image 2025-08-23 at 23.02.03_6b3039bb.jpg" 
-+            src="/0.png" 
-             alt="Agrinile Logo" 
-             className="h-16 w-auto mx-auto mb-4"
-           />
